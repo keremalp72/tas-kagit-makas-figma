33a9c3766f152f573c5fee7067aaeb0aedba4cb0
keremalp72 <55767177+keremalp72@users.noreply.github.com>
2024-11-28 20:29:31 +0300

    Add files via upload

--- assets/js/tkm.js
+++ assets/js/tkm.js
@@ -1,178 +1,62 @@
 let skor = 0;
-
-tasBtn.addEventListener("click", tasHamlesi);
-kagitBtn.addEventListener("click", kagitHamlesi);
-makasBtn.addEventListener("click", makasHamlesi);
 const gameContainer = document.querySelector(".game-btns");
 
+const hamleler = {
+  Taş: "rock.svg",
+  Kağıt: "paper.svg",
+  Makas: "scissors.svg",
+};
 
 function bilgisayarHamlesi() {
-  return ["Taş", "Kağıt", "Makas"][Math.floor(Math.random() * 3)];
-}
-function tryAgain() {
-  gameContainer.innerHTML = `<div class="game-btns">
-          <a id="kagitBtn" href="#"
-            ><img src="assets/img/paper.svg" alt=""
-          /></a>
-          <a id="makasBtn" href="#"
-            ><img src="assets/img/scissors.svg" alt=""
-          /></a>
-          <a id="tasBtn" href="#"><img src="assets/img/rock.svg" alt="" /></a>
-        </div>`;
+  const choices = ["Taş", "Kağıt", "Makas"];
+  return choices[Math.floor(Math.random() * 3)];
 }
 
-function tasHamlesi() {
-  gameContainer.innerHTML = "";
-  const hamle = bilgisayarHamlesi();
-  if (hamle == "Kağıt") {
-
-    score.innerText = skor;
-    gameContainer.innerHTML = `  <div class="choise">
-          <a id="tasBtn" href="#"><img src="assets/img/rock.svg" alt="" /></a>
-          <a id="kagitBtn" href="#"><img src="assets/img/paper.svg" alt=""/></a>
-        </div>
-        <div class="selects">
-            <h3>YOU PICKED</h3>
-            <h3>THE HOUSE PICKED</h3>
-          </div>
-          <div class="result-container">
-        <h1>YOU LOSE</h1>
-        <button id="playAgain">PLAY AGAIN</button>
-      </div>`;
-playAgain.addEventListener("click", tryAgain);
-
-  } else if (hamle == "Makas") {
+function oyunSonucu(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) return "DRAW";
+  if (
+    (playerChoice === "Taş" && computerChoice === "Makas") ||
+    (playerChoice === "Kağıt" && computerChoice === "Taş") ||
+    (playerChoice === "Makas" && computerChoice === "Kağıt")
+  ) {
     skor++;
     score.innerText = skor;
-    gameContainer.innerHTML = `  <div class="choise">
-          <a id="tasBtn" href="#"><img src="assets/img/rock.svg" alt="" /></a>
-          <a id="makasBtn" href="#"><img src="assets/img/scissors.svg" alt=""/></a>
-        </div>
-        <div class="selects">
-            <h3>YOU PICKED</h3>
-            <h3>THE HOUSE PICKED</h3>
-          </div>
-          <div class="result-container">
-        <h1>YOU WIN</h1>
-        <button id="playAgain">PLAY AGAIN</button>
-      </div>`;
-    playAgain.addEventListener("click", tryAgain);
-  } else {
-    score.innerText = skor;
-    gameContainer.innerHTML = `  <div class="choise">
-          <a id="tasBtn" href="#"><img src="assets/img/rock.svg" alt="" /></a>
-          <a id="tasBtn" href="#"><img src="assets/img/rock.svg" alt=""/></a>
-        </div>
-        <div class="selects">
-            <h3>YOU PICKED</h3>
-            <h3>THE HOUSE PICKED</h3>
-          </div>
-          <div class="result-container">
-        <h1>DRAW</h1>
-        <button id="playAgain">PLAY AGAIN</button>
-      </div>`;
+    return "YOU WIN";
   }
-  playAgain.addEventListener("click", tryAgain);
+  return "YOU LOSE";
 }
 
-function kagitHamlesi() {
-  const hamle = bilgisayarHamlesi();
-  if (hamle === "Makas") {
-    score.innerText = skor;
-    gameContainer.innerHTML = `  <div class="choise">
-          <a id="papperBtn" href="#"><img src="assets/img/paper.svg" alt="" /></a>
-          <a id="makasBtn" href="#"><img src="assets/img/scissors.svg" alt=""/></a>
-        </div>
-        <div class="selects">
-            <h3>YOU PICKED</h3>
-            <h3>THE HOUSE PICKED</h3>
-          </div>
-          <div class="result-container">
-        <h1>YOU LOSE</h1>
-        <button id="playAgain">PLAY AGAIN</button>
-      </div>`;
-    playAgain.addEventListener("click", tryAgain);
-  } else if (hamle == "Taş") {
-    skor++;
-    score.innerText = skor;
-    gameContainer.innerHTML = `  <div class="choise">
-          <a id="papperBtn" href="#"><img src="assets/img/paper.svg" alt="" /></a>
-          <a id="rockBtn" href="#"><img src="assets/img/rock.svg" alt=""/></a>
-        </div>
-        <div class="selects">
-            <h3>YOU PICKED</h3>
-            <h3>THE HOUSE PICKED</h3>
-          </div>
-          <div class="result-container">
-        <h1>YOU WIN</h1>
-        <button id="playAgain">PLAY AGAIN</button>
-      </div>`;
-    playAgain.addEventListener("click", tryAgain);
-  } else {
-    score.innerText = skor;
-    gameContainer.innerHTML = `  <div class="choise">
-          <a id="papperBtn" href="#"><img src="assets/img/paper.svg" alt="" /></a>
-          <a id="paperBtn" href="#"><img src="assets/img/paper.svg" alt=""/></a>
-        </div>
-        <div class="selects">
-            <h3>YOU PICKED</h3>
-            <h3>THE HOUSE PICKED</h3>
-          </div>
-          <div class="result-container">
-        <h1>DRAW</h1>
-        <button id="playAgain">PLAY AGAIN</button>
-      </div>`;
-  }
-  playAgain.addEventListener("click", tryAgain);
+function oyunBaslat(playerChoice) {
+  const computerChoice = bilgisayarHamlesi();
+  const result = oyunSonucu(playerChoice, computerChoice);
+
+  gameContainer.innerHTML = `
+    <div class="choise">
+      <a href="#"><img src="assets/img/${hamleler[playerChoice]}" alt="" /></a>
+      <a href="#"><img src="assets/img/${hamleler[computerChoice]}" alt="" /></a>
+    </div>
+    <div class="selects">
+      <h3>YOU PICKED</h3>
+      <h3>THE HOUSE PICKED</h3>
+    </div>
+    <div class="result-container">
+      <h1>${result}</h1>
+      <button id="playAgain">PLAY AGAIN</button>
+    </div>
+  `;
+
+  playAgain.addEventListener("click", oyunuSifirla);
 }
-function makasHamlesi() {
-  const hamle = bilgisayarHamlesi();
-  if (hamle == "Taş") {
-    score.innerText = skor;
-    gameContainer.innerHTML = `  <div class="choise">
-          <a id="scissorsBtn" href="#"><img src="assets/img/scissors.svg" alt="" /></a>
-          <a id="rockBtn" href="#"><img src="assets/img/rock.svg" alt=""/></a>
-        </div>
-        <div class="selects">
-            <h3>YOU PICKED</h3>
-            <h3>THE HOUSE PICKED</h3>
-          </div>
-          <div class="result-container">
-        <h1>YOU LOSE</h1>
-        <button id="playAgain">PLAY AGAIN</button>
-      </div>`;
-    playAgain.addEventListener("click", tryAgain);
-  } else if (hamle == "Kağıt") {
-    skor++;
-    score.innerText = skor;
-    gameContainer.innerHTML = `  <div class="choise">
-          <a id="scissorsBtn" href="#"><img src="assets/img/scissors.svg" alt="" /></a>
-          <a id="paperBtn" href="#"><img src="assets/img/paper.svg" alt=""/></a>
-        </div>
-        <div class="selects">
-            <h3>YOU PICKED</h3>
-            <h3>THE HOUSE PICKED</h3>
-          </div>
-          <div class="result-container">
-        <h1>YOU WIN</h1>
-        <button id="playAgain">PLAY AGAIN</button>
-      </div>`;
-    playAgain.addEventListener("click", tryAgain);
-  } else {
-    score.innerText = skor;
-    console.log("berabere");
-    gameContainer.innerHTML = `  <div class="choise">
-          <a id="scissorsBtn" href="#"><img src="assets/img/scissors.svg" alt="" /></a>
-          <a id="scissorsBtn" href="#"><img src="assets/img/scissors.svg" alt=""/></a>
-        </div>
-        <div class="selects">
-            <h3>YOU PICKED</h3>
-            <h3>THE HOUSE PICKED</h3>
-          </div>
-          <div class="result-container">
-        <h1>DRAW</h1>
-        <button id="playAgain">PLAY AGAIN</button>
-      </div>`;
-  }
-  playAgain.addEventListener("click", tryAgain);
+
+function oyunuSifirla() {
+  gameContainer.innerHTML = `
+    <a id="kagitBtn" href="#"><img src="assets/img/paper.svg" alt=""/></a>
+    <a id="makasBtn" href="#"><img src="assets/img/scissors.svg" alt=""/></a>
+    <a id="tasBtn" href="#"><img src="assets/img/rock.svg" alt="" /></a>
+  `;
+  kagitBtn.addEventListener("click", () => oyunBaslat("Kağıt"));
+  makasBtn.addEventListener("click", () => oyunBaslat("Makas"));
+  tasBtn.addEventListener("click", () => oyunBaslat("Taş"));
 }
+
+oyunuSifirla();
